ec541d4d47c9650c0da91055fb0560438e0d3b55
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-03-31 12:51:44 +0530

    feat: Phase 7.3 prerequisite - implement tree parsing

--- internal/object/tree.go
+++ internal/object/tree.go
@@ -2,6 +2,7 @@ package object
 import (
 	"fmt"
 	"encoding/hex"
+	"bytes"
 )
 
 type TreeEntry struct {
@@ -33,35 +34,40 @@ func (t *Tree) Serialize() ([]byte, error) {
 	return append([]byte(header), content...), nil
 }
 
-func ParseTree(data []byte)(*Tree, error){
+func ParseTree(data []byte) (*Tree, error) {
 	var entries []TreeEntry
-	i:=0
-	for i<=len(data){
-
-		//space between the mode and the filename
-		spaceIndex:= bytes.IndexByte(data[i:], ' ')
+	i := 0
+	for i < len(data) {
+		// 1. Find the space between "mode" and "filename"
+		spaceIndex := bytes.IndexByte(data[i:], ' ')
 		if spaceIndex == -1 {
 			break
 		}
 		mode := string(data[i : i+spaceIndex])
-		i += spaceIndex+1
-		
-		//the null character after the filename
-		nullIndex:= bytes.IndexByte(data[i:], 0)
+		i += spaceIndex + 1
+
+		// 2. Find the null terminator after "filename"
+		nullIndex := bytes.IndexByte(data[i:], 0)
 		if nullIndex == -1 {
 			break
 		}
 		name := string(data[i : i+nullIndex])
-		i+= nullIndex+1
+		i += nullIndex + 1
 
+		// 3. The next 20 bytes are the binary SHA-1 hash
+		if i+20 > len(data) {
+			break
+		}
 		hashBytes := data[i : i+20]
 		hashHex := hex.EncodeToString(hashBytes)
 		i += 20
 
-		entries = append(entries, TreeEntry {
+		entries = append(entries, TreeEntry{
 			Mode: mode,
 			Name: name,
 			Hash: hashHex,
 		})
 	}
 	return &Tree{Entries: entries}, nil
+}
+
